dde8183742d74ebd93ac283fa46d782e0f79b530
Faustas V <55922917+prOcenTas@users.noreply.github.com>
2022-04-14 10:53:54 +0200

    initial commit
    Initial commit with: tile generator, player movement, player animation, camera work, ladder mechanics, jump boost mechanics

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpSpeed = 11.5f;
+    [SerializeField] private float climbSpeed = 5f;
+    
+    private Vector2 moveInput;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    private CapsuleCollider2D myBodyCollider;
+    private BoxCollider2D myFeetCollider;
+    private float gravityStart;
+    
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityStart = myRigidbody.gravityScale;
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        ClimbLadder();
+    }
+    
+
+    // void FixedUpdate() //have to apply force every fixed frame
+    // {
+    //     Run();
+    // }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * speed, myRigidbody.velocity.y); //keeping whatever the velocity on y is
+        myRigidbody.velocity = playerVelocity;
+        
+        bool playerMovinHorizontal = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerMovinHorizontal);
+    }
+    
+    private void FlipSprite()
+    {
+        bool playerMovinHorizontal = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerMovinHorizontal)
+        {
+            transform.localScale = new Vector2(Math.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+    
+    private void ClimbLadder()
+    {
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            myAnimator.SetBool("isClimbing", false);
+            myRigidbody.gravityScale = gravityStart;
+            return;
+        }
+
+        Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+        //no gravity when on the lader, so that the player does not slide off while not moving
+        myRigidbody.gravityScale = 0;
+        
+        bool playerMovinVertical = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerMovinVertical);
+    }
+}
